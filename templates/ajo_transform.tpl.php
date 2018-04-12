3fdaff79f3a58ea98620ec2af2517045b4511d1e
Eileen <ekoven@gmail.com>
2018-04-11 20:19:09 -0400

    Edited fields. Saving nodes now works.

--- templates/ajo_transform.tpl.php
+++ templates/ajo_transform.tpl.php
@@ -12,6 +12,7 @@ $item['teaser'] = "Teaser stuff here.";
 $item['take_home_points'] = "Take-home points. Where do they go?";
 $item['references'] = "References. Get samples.";
 $item['digital_object_id'] = "45ADg";
+$item['author_disclosure'] = null;
 //todo: check key info
 
 //todo: figures_tables will need to be one long text of code. how to set
@@ -31,13 +32,13 @@ $figures_html = "";
 
 
 //todo: check format- will it be comma delimited?
-$item['authors'] = "Brian E. Ward, MD, Joshua S. Dines, MD";
+$item['authors'] = "Brian E. Ward, MD; Joshua S. Dines, MD";
 //check how to add new subcategories-where should they fall under? that's their own business
 $item['topics'] = "Knee, Imaging";
 $item['sections'] = "Surgery, Research, Studies";
 
 $values = array(
-    'type' => 'Article',
+    'type' => 'article',
     'uid' => 1,
     'status' => 1,
     'comment' => 1,
@@ -52,12 +53,13 @@ $ewrapper->title->set($item['title']);
 // Setting the body is a bit different from other properties or fields
 $ewrapper->body->set(array('value' => $item['body']));
 //n.b. body isn't set with summary, it's a different field
-$ewrapper->body->summary->set($item['summary']);
-$ewrapper->field_article_summary->set($item['summary']);
+//$ewrapper->body->summary->set($item['summary']);
+//the abstract is contained in the summary field
+$ewrapper->field_article_summary->set(array('value' => $item['summary']) );
 
 //Other fields that are text
 //TODO: install feature on local installation
-$ewrapper->field_article_teaser->set($item['teaser']);
+$ewrapper->field_article_teaser->set(array('value' => $item['teaser']));
 $ewrapper->field_article_doi->set($item['digital_object_id']);
 //n.b.Legacy authors is a rich text that contains author disclosure
 $ewrapper->field_article_legacy_authors->set($item['author_disclosure']);
@@ -68,46 +70,48 @@ $ewrapper->field_article_legacy_authors->set($item['author_disclosure']);
   //also check if entity type is file or image
 
   $file_efq = new EntityFieldQuery();
+  $filename = "figure1.jpg";
   $file_efq->entityCondition('entity_type', 'file')
     ->propertyCondition('filename', $filename);
   $file_efq_result = $file_efq->execute();
+  $fid = 0;
   if (count($file_efq_result)) {
     // This will only add the first match from EFQ for this filename
     $fid = array_pop($file_efq_result['file'])->fid;
   }
-  // Set both file and title separately.
+  // Set both file and title separately. TODO: need actual file
   $image_obj = file_load($fid);
-  $ewrapper->field_article_medium_image->file = $image_obj;
+  //$ewrapper->field_article_medium_image->file = $image_obj;
   //todo: get title from caption- ['big_image']['title'] 
-  $ewrapper->field_article_medium_image->title = $title;
+  //$ewrapper->field_article_medium_image->title = $title;
 
 //$ewrapper->field_article_medium_image->set();
 //$ewrapper->field_article_big_image->set();
 
 
 //Inside the article- all long text fields. Key Info = Take Home Points
-$ewrapper->field_article_key_info->set($item['take_home_points']);
+$ewrapper->field_article_key_info->set(array('value' => $item['take_home_points']));
 
 //TODO: Figures and tables requires several fields. We're going to have to add something in the $items
-$ewrapper->field_article_figures_tables->set("Code that deals with figures and tables. Will be munged from several things");
+$ewrapper->field_article_figures_tables->set(array('value' => "Code that deals with figures and tables. Will be munged from several things"));
 //TODO: What is the multimedia going to be? How do they want to insert it?
-$ewrapper->field_article_multimedia->set("some code here that inserts multimedia");
-$ewrapper->field_references->set($item['references']);
+//$ewrapper->field_article_multimedia->set("some code here that inserts multimedia");
+$ewrapper->field_references->set(array('value' => $item['references']));
 
 
 //Term reference and taxonomy fields
 $authors_vocab = taxonomy_vocabulary_machine_name_load('authors');
 $vid = $authors_vocab->vid;
+
 $authors_string = $item['authors'];
-//"Brian E. Ward, MD", "Joshua S. Dines, MD"
-//note that you type in just Brian E. Ward MD and it inserts the quotes on the form. Don't worry about it inless it barfs
-$authors_array = explode(', ', $authors_string);
+$authors_array = explode('; ', $authors_string);
+
 $tid_array = array();
 foreach($authors_array as $author) {
     $tid = custom_create_taxonomy_term($author, $vid);
     $tid_array[] = $tid;
 }
-$wrapper->field_article_authors->set($tid_array);
+$ewrapper->field_article_authors->set($tid_array);
 
 //n.b. we don't need to worry about taxonomic subcategories. They should add automagically
 //Insertion is another story, really
@@ -120,7 +124,9 @@ foreach($topics_array as $topic) {
     $tid = custom_create_taxonomy_term($topic, $vid);
     $tid_array[] = $tid;
 }
-$wrapper->field_article_topics->set($tid_array);
+
+
+$ewrapper->field_article_topics->set($tid_array);
 
 $sections_vocab = taxonomy_vocabulary_machine_name_load('sections');
 $vid = $sections_vocab->vid;
@@ -131,7 +137,8 @@ foreach($sections_array as $section) {
     $tid = custom_create_taxonomy_term($section, $vid);
     $tid_array[] = $tid;
 }
-$wrapper->field_article_sections->set($tid_array);
+var_dump($tid_array);
+$ewrapper->field_article_sections->set($tid_array);
 
 
 
@@ -141,7 +148,7 @@ $wrapper->field_article_sections->set($tid_array);
 // There is some suggestion that the 'true' argument is necessary to
 // the entity save method to circumvent a bug in Entity API. If there is
 // such a bug, it almost certainly will get fixed, so make sure to check.
-//$ewrapper->save();
+$ewrapper->save();
 
 
 
@@ -154,8 +161,7 @@ $wrapper->field_article_sections->set($tid_array);
 
  ?>
 
-ASK ABOUT THESE:
-field_article_subhead	Will there be a subhead?	
+
 Author Bio	field_author_bio	
 Article Source	field_article_source
 
@@ -165,7 +171,9 @@ field_article_citation_override  Text
 
 <?php 
 function custom_create_taxonomy_term($name, $vid) {
+
     $term_name = taxonomy_get_term_by_name($name);
+    //var_dump($term_name);
     if (is_array($term_name)) {
       $term_name = array_values($term_name)[0];
       if (isset($term_name->name)) {
